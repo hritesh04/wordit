75ccec94c759f083f9c1b9802a4901b7f3135cf7
hritesh04 <hritesh04.dev@gmail.com>
2024-08-22 15:44:13 +0530

    url changes

--- apps/web/src/socket.ts
+++ apps/web/src/socket.ts
@@ -1,4 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? process.env.BACKEND_URL : "http://localhost:3000/";
+const URL = import.meta.env.NODE_ENV === 'production' ? import.meta.env.BACKEND_URL : "http://localhost:3000/";
+console.log(URL)
 export const socket = io(URL!);

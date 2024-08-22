4546c078bfaea04200e44bed8b55624d7502d924
hritesh04 <hritesh04.dev@gmail.com>
2024-08-22 15:58:25 +0530

    vite fix

--- apps/web/src/socket.ts
+++ apps/web/src/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.NODE_ENV === 'production' ? import.meta.env.BACKEND_URL : "http://localhost:3000/";
-console.log(URL)
+const URL = import.meta.env.MODE === 'production' ? import.meta.env.BACKEND_URL : "http://localhost:3000/";
+console.log(import.meta.env)
 export const socket = io(URL!);

5dfc336a533be788f44553d445459f753ffc42d7
hritesh04 <hritesh04.dev@gmail.com>
2024-08-22 16:25:02 +0530

    added vercel analytics

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
+import { Analytics } from "@vercel/analytics/react"
 import Routes from './routes/Routes.tsx'
 import { Toaster } from 'sonner'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Analytics />
     <RouterProvider router={Routes} />
     <Toaster richColors position='top-center'/>
   </React.StrictMode>,

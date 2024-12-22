10963634387a97bab0489b22804b4a54ed9c99a4
hritesh04 <hritesh04.dev@gmail.com>
2024-12-22 22:08:53 +0530

    increase connection timeout

--- apps/web/src/socket.ts
+++ apps/web/src/socket.ts
@@ -1,4 +1,6 @@
 import { io } from 'socket.io-client';
 
 const URL = import.meta.env.VITE_MODE === 'production' ? import.meta.env.VITE_BACKEND_URL : "http://localhost:3000/";
-export const socket = io(URL!);
+export const socket = io(URL!, {
+    timeout: 10000,
+});
